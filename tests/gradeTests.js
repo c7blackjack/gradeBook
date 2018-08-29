3ec693ffe391cd61ceec51a3d1c282e59b33f7dc
TravisLloyd <jackson231@Traviss-iMac.local>
2018-08-28 20:46:57 -0700

    added an average grade function

--- tests/gradeTests.js
+++ tests/gradeTests.js
@@ -2,10 +2,17 @@ var book = require('../lib/grades').book;
 
 exports["Can add new Grade"] = function (test) {
   book.addGrade(90);
+  book.addGrade(70);
   var count = book.getCountOfGrades();
 
-  test.equal (count, 1);
-
-
+  test.equal (count, 2);
   test.done() ;
+};
+
+exports[`Can average all grades`] = function (test){
+  var aveGrades = book.averageGrades();
+
+  test.ok(aveGrades);
+
+  test.done();
 }
