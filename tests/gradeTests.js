8fb6c7a7e0bfe1a3941c5b34140a80362c544e2c
TravisLloyd <jackson231@Traviss-iMac.local>
2018-08-28 20:27:30 -0700

    Added methods and functionality to adding grades to gradeBook and returning a count

--- tests/gradeTests.js
+++ tests/gradeTests.js
@@ -1,1 +1,11 @@
-var book = require('../lib/grades');
+var book = require('../lib/grades').book;
+
+exports["Can add new Grade"] = function (test) {
+  book.addGrade(90);
+  var count = book.getCountOfGrades();
+
+  test.equal (count, 1);
+
+
+  test.done() ;
+}
