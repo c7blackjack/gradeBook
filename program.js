5b50a966161ea124e402fb92ef650bc5d5ca9428
TravisLloyd <jackson231@Traviss-iMac.local>
2018-08-28 22:35:12 -0700

    Installed/scripted in express

--- program.js
+++ program.js
@@ -1,13 +1,20 @@
 var book = require(`./lib/grades`).book;
+var express = require(`express`);
+var app = express();
 
-for(var i = 2; i < process.argv.length; i += 1) {
-  book.addGrade(parseInt(process.argv[i]));
-}
+app.get("/", function(req, res){
+  res.send("Hello");
+});
 
-console.log(`There are ` + book.getCountOfGrades() +  ` grades currently in your gradebook.`);
+app.get("/grade", function(req, res) {
+  var grades = req.query.grades.split(",");
+  for(var i = 0; i < grades.length; i++ ) {
+    book.addGrade(parseInt(grades[i]))
+  }
+  var average = book.averageGrades();
+  var letter = book.getLetterGrade();
+  res.send(`You average is ` + average + ` with a letter grade of ` + letter );
+})
 
-
-var aveGrade = book.averageGrades();
-
-
-console.log(`The class average of these grades is ` + Math.round(aveGrade));
+app.listen(3000);
+console.log("Server ready...");
